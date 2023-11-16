4aa35b34225540b3383a1098aaf592537c60efc1
alifarhat-eurus <ali.farhat@eurustechnologies.com>
2023-11-16 18:03:19 +0500

    nodeapp v6

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(' Hello, baroooo :D ');
+    res.send(' Hello, \n Nouman :D ');
 });
 
 const port = 3000;
